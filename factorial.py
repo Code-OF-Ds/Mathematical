d793470da04281c221acf8c65b3869309f3521ef
KKSawarn <kksawarn@gmail.com>
2022-08-06 16:14:27 +0530

    factorial using recursion vvip solution.

--- factorial.py
+++ factorial.py
@@ -1,12 +1,19 @@
 # factorial
+# class Solution:
+#     def fact(self,n):
+#         res =1
+#         if n ==0:
+#             res= res*1
+#         else:
+#             res = n*self.fact(n-1)
+#         return res
+
+# obj = Solution()
+# print(obj.fact(4))
+
+# VIP Solution
 class Solution:
     def fact(self,n):
-        res =1
-        if n ==0:
-            res= res*1
-        else:
-            res = n*self.fact(n-1)
-        return res
-
+        return 1 if n ==0 else n*self.fact(n-1)
 obj = Solution()
-print(obj.fact(4))
+print(obj.fact(6))
